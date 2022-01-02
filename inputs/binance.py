e0e5674aad53e4c43f26d41cdb72d6f82b2beef5
Dedalum <ha.lagrange9000@gmail.com>
2022-01-02 19:02:27 +0100

    Add investments and orders for binance input

--- inputs/binance.py
+++ inputs/binance.py
@@ -7,8 +7,10 @@ from typing import List
 from binance.spot import Spot
 from model.account import Account
 from model.investment import Investment
+from model.result import Result
+from model.trading_order import TradingOrder
 
-from .base_client import BaseClient
+from .base_client import BaseClient, BaseParser
 
 
 class Client(BaseClient):
@@ -21,23 +23,64 @@ class Client(BaseClient):
         super().__init__(api_key, api_secret)
         self.spot = Spot(key=self.api_key, secret=self.api_secret)
 
-    def get_data(self) -> dict:
+    def _get_account_snapshot(self) -> dict:
         data = {}
-        data["account_snapshot"] = self.spot.account_snapshot("SPOT")
+        data["account_snapshot"] = self._check_response(
+            self.spot.account_snapshot("SPOT")
+        )
+
         return data
 
+    def _get_account_orders(self, investments) -> dict:
+        data = []
+        for asset in investments:
+            data.append(self._check_response(self.spot.get_orders(asset.label)))
 
-class Parser:
+        return data
+
+    def get_result(self):
+        investments = self.get_investments(self._get_account_snapshot())
+        trading_orders = self.get_orders(self._get_account_orders(investments))
+
+        account = self.parser.build_accounts({})
+        account.investments = investments
+        account.trading_orders = trading_orders
+
+        result = Result()
+        result.accounts = [account]
+        return result
+
+    def get_accounts(self, data: dict) -> dict:
+        return self.parser.build_accounts(data)
+
+    def get_orders(self, data: dict) -> dict:
+        return self.parser.build_orders(data)
+
+    def get_investments(self, data: dict) -> dict:
+        return self.parser.build_investments(data)
+
+    def _check_response(self, response: dict) -> dict:
+        if response.get("code") != "200":  # > 299 and < 200
+            print("Error querying data: {}".format(response.get("msg")))
+            return {}
+        return response
+
+
+class Parser(BaseParser):
     """
     Data parser for the Binance API
     """
 
-    def get_account(self, data: dict) -> List[Account]:
+    def build_accounts(self, data: dict) -> Account:
+        # TODO: method name is plural but we only return 1 Account
         account = Account()
 
-        return [account]
+        return account
 
-    def get_investments(self, data: dict) -> List[Investment]:
+    def build_orders(self, data: dict) -> List[TradingOrder]:
+        pass
+
+    def build_investments(self, data: dict) -> List[Investment]:
         investments = []
         invs_data = data.get("snapshotVos")[0]
 
--- tests/test_binance.py
+++ tests/test_binance.py
@@ -7,7 +7,7 @@ from inputs.binance import Parser
 from model.account import Account
 
 
-class TestBinance:
+class TestBinanceParser:
     def test_get_investments(self, account_snapshot):
         parser = Parser()
         investments = parser.get_investments(account_snapshot)
@@ -15,6 +15,15 @@ class TestBinance:
         assert investments[0].label == "BTC"
         assert investments[1].label == "ETH"
         assert investments[2].label == "EUR"
+        # TODO TO BE CONTINUED
+
+    def test_get_trading_orders(self, account_orders):
+        parser = Parser
+        trading_orders = parser.get_trading_orders(account_orders)
+
+        assert trading_orders[0].label == "LTCBTC"
+        assert trading_orders[0].id == 1
+        assert trading_orders[0].order_side == "BUY"  # TradingOrderSide.BUY
 
 
 @pytest.fixture
@@ -42,3 +51,32 @@ def account_snapshot():
             }
         ],
     }
+
+
+@pytest.fixture
+def account_orders():
+    """
+    https://binance-docs.github.io/apidocs/spot/en/#current-open-orders-user_data
+    """
+    return [
+        {
+            "symbol": "LTCBTC",
+            "orderId": 1,
+            "orderListId": -1,  # Unless OCO, the value will always be -1
+            "clientOrderId": "myOrder1",
+            "price": "0.1",
+            "origQty": "1.0",
+            "executedQty": "0.0",
+            "cummulativeQuoteQty": "0.0",
+            "status": "NEW",
+            "timeInForce": "GTC",
+            "type": "LIMIT",
+            "side": "BUY",
+            "stopPrice": "0.0",
+            "icebergQty": "0.0",
+            "time": 1499827319559,
+            "updateTime": 1499827319559,
+            "isWorking": True,
+            "origQuoteOrderQty": "0.000000",
+        }
+    ]
